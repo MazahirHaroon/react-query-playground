34eb1efc318bc39875cbc21e7b0cf96d02a3b0fa
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-03-07 11:10:13 +0530

    Invoked the FetchPost API function using useQuery hook utilizing value returned like isPending and the data itself.

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { fetchPost } from '@api/post';
+
 import { SearchKeyWord } from '@interfaces/post';
+
 import './index.css';
 
 const Post = () => {
   const [searchKeyword, setSearchKeyword] = useState<SearchKeyWord>({
     userId: '',
+  });
+
+  const { isPending, data: posts } = useQuery({
+    queryKey: [['posts']],
+    queryFn: () => fetchPost(),
   });
 
   return (
@@ -39,26 +49,23 @@ const Post = () => {
           </div>
           <button className='primary-button'>Add Post</button>
         </div>
+        <h2 className='home-secondary-title'>Posts</h2>
         <div className='post-list'>
-          <h2 className='home-secondary-title'>Posts</h2>
-          <div className='post-item'>
-            <h3>{'Post Title'}</h3>
-            <p>
-              <strong>ID:</strong> {'Post ID'}
-            </p>
-            <p>
-              <strong>User ID:</strong> {'Post Title'}
-            </p>
-          </div>
-          <div className='post-item'>
-            <h3>{'Post Title'}</h3>
-            <p>
-              <strong>ID:</strong> {'Post ID'}
-            </p>
-            <p>
-              <strong>User ID:</strong> {'Post Title'}
-            </p>
-          </div>
+          {isPending ? (
+            <h2 className='home-tertiary-title'>Loading...!</h2>
+          ) : (
+            posts?.map((post) => (
+              <div className='post-item'>
+                <h3>{post.title}</h3>
+                <p>
+                  <strong>ID:</strong> {post.id}
+                </p>
+                <p>
+                  <strong>User ID:</strong> {post.userId}
+                </p>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </div>
